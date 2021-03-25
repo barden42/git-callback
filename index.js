e06b43d09a9d3581e17fe298e4ac10da5ae3c220
Bardenet <quentin.bardenet@gmail.com>
2021-03-25 16:03:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-const elements = ["FrUitS", "LeGUMeS", "AnimAuX", "VoiTURes"];
-
-// function de heaut niveau "transform"
-// la fonction permet de parcourir le tableau
-// appliquer une transformation sur tous les elements du tableaux
-// retourne un nouveau tableau contenant les elements tranformés
-
-// en minuscule
-// en majuscule
-// camel case
-
-const transform = (array, callback) => {
-  const results = [];
-  for (let i = 0; i < array.length; i++) {
-    results.push(callback(array[i]));
-  }
-  return results;
-};
+const elements = ["FrUitS", "LeGUMeS", "AnimAuX", "VoiTURes", "HuMAINs];
 
 const toLowerCase = (name) => {
   return name.toLowerCase();
@@ -25,4 +8,12 @@ const toUpperCase = (name) => {
   return name.toUpperCase();
 };
 
-const tableauTransforme = transform(elements, toUpperCase);
+const transform = (array, callback) => {
+  const results = [];
+  for (let i = 0; i < array.length; i++) {
+    results.push(callback(array[i]));
+  }
+  return results;
+};
+
+const tableauTransforme = transform(elements, toLowerCase);
